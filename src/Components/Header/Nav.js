85e6529ab68e3291f455c8caf31f61bee0f5bd2f
Emranul_farhad <emranulfarhad@gmail.com>
2022-08-14 19:32:43 +0600

    hero part done

---
+++ src/Components/Header/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+
+const Nav = () => {
+  return (
+    <div>
+        <div className='flex justify-between p-[2rem] uppercase helo text-3xl'>
+            <div>  Menue </div>
+            <div className='special'>  FLIRTY FLOWERS </div>
+            <div>  Cart </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Hero.css'
+
+
+const Hero = () => {
+
+    return (
+        <section className=' mt-5'>
+            <ul className='intro'>
+                <li>Intro</li>
+                <li>about</li>
+                <li>featured </li>
+            </ul>
+            <p className='art uppercase text-center font-extrabold'>Tree 
+            pla<span className='special'>n</span>ets </p>
+           
+            <div>
+               
+            </div>
+        </section>
+    )
+}
+
+export default Hero
