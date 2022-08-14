284e82b4436b069fc3d7c63006ad362af0f04070
Emranul_farhad <emranulfarhad@gmail.com>
2022-08-15 01:22:55 +0600

    about done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './Components/Header/Nav';
 import Hero from './Components/Hero/Hero';
 import Features from './Components/Features/Features';
+import About from './Components/About/About';
 
 
 
@@ -12,8 +13,12 @@ function App() {
     <div className="App">
       <Nav></Nav>
       <Hero></Hero>
-      <Features></Features>
-      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, autem.</h3>
+     <div className='mt-[120px]'>
+     <Features></Features>
+     </div>
+     <div className='mt-[50px]'>
+      <About></About>
+     </div>
     </div>
   );
 }
---
+++ src/Components/About/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './About.css'
+
+
+const About = () => {
+
+  return (
+    <div>
+        <div className='p-[3rem]'>
+            <h1 className='uppercase  mission'>Mission</h1>
+            <p className='mission-about font-extrabold'>On a mission to plant 1 trillion trees by 2050
+            There is no solving climate change without solving deforestation. Discover our Master Plan on how we aim to achieve this monster goal.
+            There is no solving climate change without solving deforestation. Discover our Master Plan on how we aim to achieve this monster goal.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
