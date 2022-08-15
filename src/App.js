10f2530afa406c98ad2479e75c63ec4e8a87e0f4
Emranul_farhad <emranulfarhad@gmail.com>
2022-08-15 23:42:30 +0600

    gallery

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './Components/Header/Nav';
 import Hero from './Components/Hero/Hero';
 import Features from './Components/Features/Features';
 import About from './Components/About/About';
+import Gallery from './Components/Gallery/Gallery';
 
 
 
@@ -13,12 +14,16 @@ function App() {
     <div className="App">
       <Nav></Nav>
       <Hero></Hero>
-     <div className='mt-[120px]'>
-     <Features></Features>
-     </div>
-     <div className='mt-[50px]'>
-      <About></About>
-     </div>
+      <div className='mt-[120px]'>
+        <Features></Features>
+      </div>
+      <div className='mt-[50px]'>
+        <About></About>
+      </div>
+      <div>
+        <Gallery></Gallery>
+      </div>
+
     </div>
   );
 }
